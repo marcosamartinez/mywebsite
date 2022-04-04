65b95fdc3889b23e5d7d5dc060d60a13b5da039d
marcos <marcosmartinz176@gmail.com>
2022-04-04 10:42:00 -0400

    Done with about me part

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,9 +11,22 @@ const About = () => {
       <div>
         <h2>About Me</h2>
         <p>
-          My name is Marcos. I'm a young Developer just getting started on my
-          journey.
+          My name is Marcos Martinez. I'm a young Developer just getting started
+          on my journey. As a kid growing up you see a lot of things. For me
+          homeless people, fighting, crime, was an everyday thing. Now what I
+          took from this was that I could be the diamond in the dirt. The first
+          of my y family to make it out. Not only from the hood, but to be set
+          for life and not worry about any bills. Now this is not the only
+          reason Im hungry, its hard to see your mom have goals and aspirations
+          that shes not able to complete because she had kids and is a single
+          mother. Also a sister that has a disability that will soon alter her
+          life forever. This means I have to step up to the plate and provide
+          for them both, they are the ones im going on this journey for.
         </p>
+      </div>
+      <div>
+        <h2>Where Im from</h2>
+        <p></p>
       </div>
     </div>
   );

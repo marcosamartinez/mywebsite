d9162d4111c60d5a742d37e530534c349418a0b0
marcos-digitol <marcos@digitol.io>
2026-02-16 21:05:58 -0500

    +1

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import "./about.css";
-import aboutHeadshot from "../../assets/about-headshot.png";
-import skyline5 from "../../assets/skyline5.jpeg";
 
 const About = () => {
-  // Styled Componentes
-
   return (
     <div className="about-page">
       <div className="about-me">
         <h2>About Me</h2>
-        <img className="marcos-picture" src={aboutHeadshot} alt="Marcos Martinez" />
         <p>
         As a dev I want to express my Love for Technology! Tackling any task that comes forward from debugging to pushing out a component to millions of people. I'm a person that will see things to the end. Until its built in the most efficient way I will not stop working towards it. Creating a website that's beautiful not only in the front end but also in the back end. Working on ways for both ends to verify its the correct person logging in also making it run smooth with the best user experience. 
 
@@ -21,7 +16,6 @@ My story is very uncommon in the tech field. I'm a kid that grew up with a famil
       </div>
       <div className="where-im-from">
         <h2>Concreate Jungle</h2>
-        <img className="new-york" src={skyline5} alt="New York City" />
         <p>
           {" "}
           Im from New York City. Washington Heights exactly where you see a lot
